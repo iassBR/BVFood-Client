e6fe5181f69f69adad2682dff930f66024ddfe5f
Igor Alexander Silva <igor.alexander.silva@gmail.com>
2021-04-07 14:05:27 -0400

    initial commit

---
+++ src/layouts/AuthTemplate.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <!-- Page Content -->
+    <div class="container container-body">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ src/layouts/BaseTemplate.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <route-view></route-view>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+@import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css");
+</style>
